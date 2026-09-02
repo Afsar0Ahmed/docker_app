00a2d198ceaad4ec7418701cfd84bdc0006fd295
Afsar0Ahmed <sk4885426@gmail.com>
2026-09-02 20:59:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-    <h1>Hello This is Tutrial of ract Application</h1>
+    <h1>Hello All the ci/cd Pipeline work....</h1>
     </div>
   )
 }
